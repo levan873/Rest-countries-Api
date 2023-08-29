a14167ff7a0bb3a97b0874231e6ac0eb8cf6c63f
unknown <nikakhaburzania18@gmail.com>
2023-08-29 12:43:01 +0400

    smth changed

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 const Nav: React.FC = () => {
   return (
     <nav className="">
-      <Link to="http://localhost:3000/"> 
+      <Link to=''> 
          <h1 className="bg-transparent text-3xl font-semibold ">Where in the world?</h1>
           </Link>
     </nav>
